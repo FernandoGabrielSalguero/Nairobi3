e10c4cf2ca46e0e337ec539b0637f52f15686cf0
FernandoGabrielSalguero <149271548+FernandoGabrielSalguero@users.noreply.github.com>
2025-11-09 14:51:22 -0300

    main

--- views/admin/admin_carta_view.php
+++ views/admin/admin_carta_view.php
@@ -21,9 +21,9 @@ $email = $user['email'] ?? 'Sin email';
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 
-    <!-- Framework desde CDN -->
-    <link rel="stylesheet" href="https://www.fernandosalguero.com/cdn/assets/css/framework.css">
-    <script src="https://www.fernandosalguero.com/cdn/assets/javascript/framework.js" defer></script>
+    <!-- Framework Success desde CDN -->
+    <link rel="stylesheet" href="https://framework.impulsagroup.com/assets/css/framework.css">
+    <script src="https://framework.impulsagroup.com/assets/javascript/framework.js" defer></script>
 
     <style>
         /* Inline: estilos mínimos sin romper el CDN */
